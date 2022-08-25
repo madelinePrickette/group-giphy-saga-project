f0c50567cc283f2081c2b988632057bb50bf6e0c
Taylor Moe <taylormoe93@gmail.com>
2022-08-25 11:43:13 -0500

    App.js router complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import {HashRouter as Router, Route} from 'react-router-dom'
+
+//IMPORT COMPONENTS
+import Favorites from '../Favorites/Favorites';
+
 
 function App(props) {
-  return (
+ 
+
+
+
+
+  // ROUTERS
+return ( 
+  <Router>
     <div>
       <h1>Giphy Search!</h1>
     </div>
-  );
-}
+
+    <div className='App'>
+      <header className='App-header'>
+        <h1 className='App-title'>Giphy Saga Group Project</h1>
+      </header>
+
+      <p>Find and favorite gifs!</p>
+      
+      {/* SEARCH */}
+      <Route path="/search">
+        <Search />
+      </Route>
+
+      {/* FAVORITE */}
+      <Route path="/favorite">
+        <Favorite />
+      </Route>
+
+    </div>
+  </Router>
+); // end ROUTERS
+
+}// end APP
+
+
 
 export default App;
