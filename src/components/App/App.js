eb86afa5e9321055a2891eb1955a2e26a949a91d
Justin Nicholas <nicholasj964@yahoo.com>
2022-08-25 11:39:10 -0500

    search fature and store set up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Search from '../Search/Search.jsx'
 
 function App(props) {
   return (
     <div>
       <h1>Giphy Search!</h1>
+      <Search />
     </div>
   );
 }
