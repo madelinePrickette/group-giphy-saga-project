7e8a199dd614bfa067c1548c199ced236daf4f67
Madeline Prickette <madelineprickette@gmail.com>
2022-08-26 09:20:20 -0500

    fixed nav

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {HashRouter as Router, Route} from 'react-router-dom';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import ResultsList from '../ResultsList/ResultsList.jsx';
 //IMPORT COMPONENTS]
 import Favorite from '../Favorite/Favorite';
@@ -15,19 +15,23 @@ function App(props) {
   // ROUTERS
 return ( 
   <Router>
-    <div>
-      <h1>Giphy Search!</h1>
-      <Search />
-      <ResultsList />
-    </div>
-    
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Giphy Saga Group Project</h1>
       </header>
-
       <p>Find and favorite gifs!</p>
-      
+
+    <nav>
+      <ul>
+        <li>
+          <Link to='/search'>Search</Link>
+        </li>
+        <li>
+          <Link to='/favorite'>Favorites</Link>
+        </li>
+      </ul>
+    </nav>
+    
       {/* SEARCH */}
       <Route path="/search">
         <Search />
