2dae4538387c77fa68dcff98fa2872c0d6ae9678
Madeline Prickette <madelineprickette@gmail.com>
2022-08-26 10:55:41 -0500

    added comments

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const pool = require('../modules/pool');
-require('dotenv').config();
-const axios = require('axios');
+require('dotenv').config(); // needed when hiding keys to apis.
+const axios = require('axios'); // needed when making requests
 
 const router = express.Router();
 
 //GIPHY SEARCH GET REQUEST COMING FROM SERVER
-router.get('/:search', (req, res) => {
+router.get('/:search', (req, res) => { // this /:search contains what the user typed in to search
     console.log('cat');
-    console.log(req.params);
+    console.log(req.params); // this is an object with a key of search which contains a value {search: cats}
     axios.get(`http://api.giphy.com/v1/gifs/search?q=${req.params.search}&api_key=${process.env.GIPHY_API_KEY}`)
+    // this is the giphy link, it takes the object of req.params and pulls the value of search out and places it here.
+    // this also holds the giphy key
         .then( (response) => {
-            res.send(response.data)
+            res.send(response.data) // these are all the giphs recieved from the giphy api along with every version of gif. a lot of stuff.
         }).catch(err => {
-            console.log(err);
+            console.log(err); // catching an error
         })
 })
 
