d0cfa8a5510bf129e5b7015b7c47a292df699a50
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-08-26 10:55:08 -0500

    resultList, resultItem comments created

--- src/components/ResultsItem/ResultsItem.jsx
+++ src/components/ResultsItem/ResultsItem.jsx
@@ -2,9 +2,11 @@
 import {useDispatch} from 'react-redux';
 
 function ResultsItem ({pic}) {
-
+    //create an instance of dispatch
     let dispatch = useDispatch();
 
+    //favoriteGif function declared here. This function will run when the heart button is clicked.
+    //'FAVORITE_GIF' action is dispatched; the image url is sent via payload. 
     const favoriteGif = () => {
         console.log('clicked favorite')
         dispatch({
@@ -13,9 +15,12 @@ function ResultsItem ({pic}) {
     }
 
     return(
-
+        
         <>
+        {/* //we drill into the pic prop object (passed from resultsList) and display the img
+        //through the image tag.  */}
             <li><img src={pic.images.original.url}></img></li>
+        {/* when the heart button is clicked, action 'FAVORITE_GIF' action is dispatched. */}
             <button onClick={favoriteGif}>❤️Favorite</button>
         </>
         
--- src/components/ResultsList/ResultsList.jsx
+++ src/components/ResultsList/ResultsList.jsx
@@ -2,10 +2,16 @@ import ResultsItem from '../ResultsItem/ResultsItem.jsx';
 import {useSelector} from 'react-redux';
 function ResultsList () {
 
+    //we connect to the store via useSelector hook and
+    //we set a searchResult variable to the searchResult reducer
+    //searchResult is an array of objects from the search results from API.
     const searchResult = useSelector(store => store.searchResult);
     console.log('searchResult', searchResult);
 
+
     return(
+        //We map over searchResult and return the ResultsItem component
+        //we pass the entire pic object on to ResultsItem component via PROPS
         <ul>
         {searchResult.map((pic) =>{
             return (
