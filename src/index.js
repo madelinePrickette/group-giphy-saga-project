9bc58ff89b25c0db050da5b20f59527ac40c0e0d
Justin Nicholas <nicholasj964@yahoo.com>
2022-08-26 10:52:34 -0500

    aded comments to index and favorite

--- src/index.js
+++ src/index.js
@@ -10,19 +10,15 @@ import {takeEvery, put} from 'redux-saga/effects';
 
 const sagaMiddleware = createSagaMiddleware();
 
+//below are all of the listeners for dispatches sent from the client side.
 function* rootSaga() {
-
     yield takeEvery('SUBMIT_SEARCH', getResults)
     yield takeEvery('FAVORITE_GIF', favoriteGif)
     yield takeEvery('GET_FAVORITES', getFavorites);
     yield takeEvery('CHANGE_CATEGORY', getCategory);
 } 
 
-// function* submitSearch(action){
-//     // console.log('searching');
-
-// }
-
+// this gets the response from the seach and calls the set response function with this response.data as an argument.
 function* getResults(action){
     try {
         let response = yield axios.get(`/api/search/${action.payload}`)
@@ -32,7 +28,7 @@ function* getResults(action){
     }
 }
 
-
+// this function adds a gif to the favorites by posting the gif to the database. we also call get favorites to update the favorites
 function* favoriteGif(action) {
     console.log(action.payload);
     try{
@@ -43,6 +39,9 @@ function* favoriteGif(action) {
     }
 }
 
+// this function sets the category passed through when the user selects a category and submits.
+// we pass in the id of the gif and the category id we are adding. we then call get favorites to update favorites.
+// we have to send through the action.payload.favCategory as an object because it is drilled into on the server side route.
 function* getCategory(action) {
     console.log(action.payload);
     try{
@@ -54,6 +53,7 @@ function* getCategory(action) {
 
 }//end of category function
 
+// this holds the info of the search results that we pass through as a reducer to map through and render in the search page.
 const searchResult = ( state = [], action ) => {
     switch (action.type) {
         case 'SET_RESPONSE':
@@ -63,6 +63,7 @@ const searchResult = ( state = [], action ) => {
     }
 }
 
+// this function sends a get request to get all the favorites and calls set favorites, using the response.data as the argument.
 function* getFavorites(action){
     try{
         let response = yield axios.get('/api/favorite')
@@ -72,6 +73,7 @@ function* getFavorites(action){
     }
 }
 
+// this is where we hold the favorites info that we pass through as a reducer to map through and render in the favorites page.
 const favorites = ( state = [], action ) => {
     switch (action.type) {
         case 'SET_FAVORITES':
@@ -90,18 +92,11 @@ const dropdownCategory = ( state='', action ) => {
     return state;
 }
 
-
-
-//search reducer
-
-//favorites reducer
-
+// we pass through the below to be used in the useSelectors on the client side.
 const store = createStore(
     combineReducers({
         searchResult,
         favorites,
-        //search reducer
-        //favorites reducer
         favoriteGif,
         dropdownCategory
     }),
